6315ffd42894980ec0ca1b8cd82767e236914bb4
Changhun Han <shirohoo4102@gmail.com>
2022-08-11 00:56:43 +0900

    :sparkles: set up kotlin in project

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.0.0-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.0.12.RELEASE"
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("java")
+}
+
+group = "io.github.shirohoo"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+    maven(url = "https://repo.spring.io/milestone")
+    maven(url = "https://repo.spring.io/snapshot")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "kopring-library-application"
+
+pluginManagement {
+    repositories {
+        maven(url = "https://repo.spring.io/milestone")
+        maven(url = "https://repo.spring.io/snapshot")
+        gradlePluginPortal()
+    }
+}

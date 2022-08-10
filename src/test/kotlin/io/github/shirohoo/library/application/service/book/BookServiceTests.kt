f398faae2dbe92e3737164c1506516c77a05d2f8
Changhun Han <shirohoo4102@gmail.com>
2022-08-11 01:21:33 +0900

    :white_check_mark: add tests for BookService

---
+++ src/test/kotlin/io/github/shirohoo/library/application/service/book/BookServiceTests.kt
@@ -0,0 +1,76 @@
+package io.github.shirohoo.library.application.service.book
+
+import io.github.shirohoo.library.application.persistance.book.BookJpaRepository
+import io.github.shirohoo.library.application.persistance.user.UserJpaRepository
+import io.github.shirohoo.library.domain.book.Book
+import io.github.shirohoo.library.domain.user.User
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceTests @Autowired constructor(
+    private var sut: BookService,
+    private var userJpaRepository: UserJpaRepository,
+    private var bookJpaRepository: BookJpaRepository
+) {
+    @Test
+    fun `새로운 도서를 저장한다`() {
+        val newBook = Book("book title")
+        assertDoesNotThrow { sut.saveBook(newBook) }
+    }
+
+    @Test
+    fun `도서를 대출할 수 있다`() {
+        // given
+        val newUser = User("username", null)
+        userJpaRepository.save(newUser)
+
+        val newBook = Book("book title")
+        sut.saveBook(newBook)
+
+        // when and then
+        assertDoesNotThrow { sut.loanBook("username", "book title") }
+    }
+
+    @Test
+    fun `이미 대출된 도서를 대출할수는 없다`() {
+        // given
+        val newUser = User("username", null)
+        userJpaRepository.save(newUser)
+
+        val newBook = Book("book title")
+        sut.saveBook(newBook)
+
+        sut.loanBook("username", "book title")
+
+        // when and then
+        assertThrows<IllegalArgumentException>("this book has already been borrowed.") {
+            sut.loanBook("username", "book title")
+        }
+    }
+
+    @Test
+    fun `대출한 도서를 반납할 수 있다`() {
+        // given
+        val newUser = User("username", null)
+        userJpaRepository.save(newUser)
+
+        val newBook = Book("book title")
+        sut.saveBook(newBook)
+
+        sut.loanBook("username", "book title")
+
+        // when and then
+        assertDoesNotThrow { sut.returnBook("username", "book title") }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        userJpaRepository.deleteAll()
+        bookJpaRepository.deleteAll()
+    }
+}

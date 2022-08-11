ad058ad6b833135532ad42fc7aed8796bf625847
Changhun Han <shirohoo4102@gmail.com>
2022-08-12 00:01:33 +0900

    :construction: migrating UserController class from java to kotlin

---
+++ src/main/kotlin/io/github/shirohoo/library/application/controller/user/UserController.kt
@@ -0,0 +1,35 @@
+package io.github.shirohoo.library.application.controller.user
+
+import io.github.shirohoo.library.application.dto.user.UserCreateRequest
+import io.github.shirohoo.library.application.dto.user.UserResponse
+import io.github.shirohoo.library.application.dto.user.UserUpdateRequest
+import io.github.shirohoo.library.application.service.user.UserService
+import io.github.shirohoo.library.domain.user.User
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/users")
+class UserController(
+    private val userService: UserService
+) {
+    @GetMapping
+    fun getUsers(): List<UserResponse> {
+        return userService.getUsers()
+            .map(::UserResponse)
+    }
+
+    @PostMapping
+    fun saveUser(@RequestBody request: UserCreateRequest) {
+        val user = request.let { User(it.name, it.age) }
+        userService.saveUser(user)
+    }
+
+    @PutMapping
+    fun updateUsername(@RequestBody request: UserUpdateRequest) {
+        val (userId, username) = request
+        userService.updateUsername(userId, username)
+    }
+
+    @DeleteMapping
+    fun deleteUser(@RequestBody username: String) = userService.deleteUser(username)
+}
--- src/main/kotlin/io/github/shirohoo/library/application/dto/user/UserResponse.kt
+++ src/main/kotlin/io/github/shirohoo/library/application/dto/user/UserResponse.kt
@@ -1,7 +1,11 @@
 package io.github.shirohoo.library.application.dto.user
 
+import io.github.shirohoo.library.domain.user.User
+
 data class UserResponse(
     val id: Long,
     val name: String,
     val age: Int? = null,
-)
+) {
+    constructor(user: User) : this(user.id!!, user.name, user.age)
+}

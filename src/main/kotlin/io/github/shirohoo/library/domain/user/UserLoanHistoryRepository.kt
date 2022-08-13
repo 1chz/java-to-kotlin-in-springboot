a964b69ff96bd0d79a57a5922dbf2a994eb38f7c
Changhun Han <shirohoo4102@gmail.com>
2022-08-13 19:56:36 +0900

    :art: reformat UserLoanHistoryRepository

--- src/main/kotlin/io/github/shirohoo/library/domain/user/UserLoanHistoryRepository.kt
+++ src/main/kotlin/io/github/shirohoo/library/domain/user/UserLoanHistoryRepository.kt
@@ -2,6 +2,5 @@ package io.github.shirohoo.library.domain.user
 
 interface UserLoanHistoryRepository {
     fun existsBy(bookTitle: String, status: UserLoanStatus): Boolean
-
     fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
 }
